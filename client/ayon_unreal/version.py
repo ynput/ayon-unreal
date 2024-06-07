249026788f234fa82089f593a169575431f85104
Ondřej Samohel <ondrej.samohel@ynput.io>
2024-06-07 17:18:11 +0200

    :recycle: sync with ayon-core

---
+++ client/ayon_unreal/version.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+"""Package declaring unreal addon version."""
+__version__ = "0.2.0"
--- server/__init__.py
+++ server/__init__.py
@@ -2,15 +2,11 @@ from typing import Type
 
 from ayon_server.addons import BaseServerAddon
 
-from .settings import MySettings, DEFAULT_VALUES
-from .version import __version__
+from .settings import UnrealSettings, DEFAULT_VALUES
 
 
-class MyAddon(BaseServerAddon):
-    name = ""
-    title = ""
-    version = __version__
-    settings_model: Type[MySettings] = MySettings
+class UnrealAddon(BaseServerAddon):
+    settings_model: Type[UnrealSettings] = UnrealSettings
 
     async def get_default_settings(self):
         settings_model_cls = self.get_settings_model()
