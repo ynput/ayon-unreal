612cc2d2e97db1894be56cef5eefd784ef066e9f
Kayla Man <moonyuet2008@hotmail.com>
2024-09-03 14:03:35 +0800

    improve the logic in the extractor

--- client/ayon_unreal/plugins/publish/extract_fbx.py
+++ client/ayon_unreal/plugins/publish/extract_fbx.py
@@ -7,7 +7,7 @@ from ayon_core.pipeline import publish
 class ExtractFbx(publish.Extractor):
     """Extract Fbx."""
 
-    label = "Extract Fbx"
+    label = "Extract Fbx (Static Mesh)"
     hosts = ["unreal"]
     families = ["staticMesh"]
 
@@ -22,23 +22,20 @@ class ExtractFbx(publish.Extractor):
         options.set_editor_property('collision', False)
         fbx_filename = f"{instance.name}.fbx"
 
-        ar = unreal.AssetRegistryHelpers.get_asset_registry()
-
         task = unreal.AssetExportTask()
         task.exporter = fbx_exporter
         task.options = options
+        members = set(instance.data.get("members", []))
+        asset_registry = unreal.AssetRegistryHelpers.get_asset_registry()
+        for member in members:
+            task.object = asset_registry.get_asset_by_object_path(member).get_asset()
+            task.automated = True
+            task.filename = os.path.join(staging_dir, fbx_filename).replace("\\", "/")
+            task.selected = False
+            task.use_file_archive = False
+            task.write_empty_files = False
 
-        for member in instance.data.get("members"):
-            target_asset = ar.get_asset_by_object_path(member).get_asset()
-            task.object = target_asset
-        task.automated = True
-        task.filename = os.path.join(staging_dir, fbx_filename).replace("\\", "/")
-        task.prompt = True
-        task.selected = True
-        task.use_file_archive = False
-        task.write_empty_files = True
-
-        unreal.Exporter.run_asset_export_task(task)
+            unreal.Exporter.run_asset_export_task(task)
 
         if "representations" not in instance.data:
             instance.data["representations"] = []
