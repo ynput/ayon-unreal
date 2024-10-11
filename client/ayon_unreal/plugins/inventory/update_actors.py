8a0c6fd8fbdf4acda805ecb7b005bd1321929e24
Kayla Man <moonyuet2008@hotmail.com>
2024-10-11 23:39:35 +0800

    refactoring the codes to correctly collect the asset_name when comparing with the old and new asset content

--- client/ayon_unreal/plugins/inventory/update_actors.py
+++ client/ayon_unreal/plugins/inventory/update_actors.py
@@ -1,3 +1,4 @@
+import re
 import unreal
 
 from ayon_unreal.api.pipeline import (
@@ -7,6 +8,24 @@ from ayon_unreal.api.pipeline import (
     replace_geometry_cache_actors,
 )
 from ayon_core.pipeline import InventoryAction
+
+
+def find_common_parts(old_asset_name, new_asset_name):
+    # Find the common prefix
+    prefix_match = re.match(r"^(.*?)(\d+)(.*?)$", old_asset_name)
+    if not prefix_match:
+        return
+    name, _, ext = prefix_match.groups()
+
+    # Construct a dynamic pattern based on the common prefix and suffix
+    pattern = re.escape(name) + r"(\d+)" + re.escape(ext)
+
+    # Match the pattern in the second variable
+    new_version_match = re.match(pattern, new_asset_name)
+    if not new_version_match:
+        return
+
+    return new_asset_name
 
 
 def update_assets(containers, selected):
@@ -28,7 +47,8 @@ def update_assets(containers, selected):
             i
             for i in all_containers
             if (
-                i.get("asset_name") == container.get("asset_name") and
+                find_common_parts(
+                    i.get("asset_name"), container.get("asset_name")) and
                 i.get("objectName") != container.get("objectName")
             )
         ]
