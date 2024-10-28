626f530fbfdba9b68317ec94ba674a783aa3aa0b
Kayla Man <moonyuet2008@hotmail.com>
2024-10-28 22:11:36 +0800

    add generate hierarchy path function into pipeline.py

--- client/ayon_unreal/api/pipeline.py
+++ client/ayon_unreal/api/pipeline.py
@@ -1068,3 +1068,18 @@ def get_top_hierarchy_folder(path):
     # Return the first part
     parts = [part for part in path.split('/') if part]
     return parts[0]
+
+
+def generate_hierarchy_path(context, name, folder_entity,
+                            loaded_dir="{folder[path]}/{product[name]}"):
+    # Remove folder name
+    folder_name = folder_entity["name"]
+    suffix = "_CON"
+    asset_name = f"{folder_name}_{name}" if folder_name else name
+    asset_root, _ = format_asset_directory(context, loaded_dir)
+    master_dir_name = get_top_hierarchy_folder(asset_root)
+    hierarchy_dir = f"{AYON_ROOT_DIR}/{master_dir_name}"
+    tools = unreal.AssetToolsHelpers().get_asset_tools()
+    asset_dir, container_name = tools.create_unique_asset_name(asset_root, suffix="")
+    container_name += suffix
+    return asset_dir, hierarchy_dir, container_name, asset_name
