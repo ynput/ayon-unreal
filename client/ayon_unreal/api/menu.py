3671a690e7499922990eb7f633f1e4c447c06002
Chad Provencher <thechad802@gmail.com>
2025-10-29 11:34:36 -0400

    added ayon menu

--- client/ayon_unreal/api/menu.py
+++ client/ayon_unreal/api/menu.py
@@ -1,28 +1,44 @@
 import unreal
 
+@unreal.uclass()
+class AyonPythonMenuTool(unreal.ToolMenuEntryScript):
+    name = ""
+    display_name = ""
+    tool_tip = ""
+
+    def __init__(self, menu_object, section_name=""):
+        super().__init__()
+        self.init_entry(
+            owner_name="ayon_manager_id",
+            menu=menu_object.menu_name,
+            section=section_name,
+            name=self.name,
+            label=self.display_name
+        )
+        menu_object.add_menu_entry_object(self)
+
+    @unreal.ufunction(override=True)
+    def execute(self, context):
+        print(f"Context {context}")
+
+@unreal.uclass()
+class AyonToolsMenuTool(AyonPythonMenuTool):
+    name = "ayon_tools_menu"
+    display_name = "Ayon Tools"
+    tool_tip = "Launches the Ayon tools menu."
+
+    @unreal.ufunction(override=True)
+    def execute(self, context):
+        from ayon_unreal.api import tools_ui
+        tools_ui.show_tools_dialog()
 
 def init_ayon_menu():
-    print("Menu Init...")
     menus = unreal.ToolMenus.get()
-    section_name = "halon_ayon_section"
-    command = (
-        "from ayon_unreal.api import tools_ui;"
-        "tools_ui.show_tools_dialog()"
+    main_menu = menus.find_menu("LevelEditor.MainMenu")
+    ayon_menu = main_menu.add_sub_menu(
+        owner="ayon_manager_id",
+        section_name='',
+        name="ayon_tools",
+        label="Ayon"
     )
-
-    level_menu_bar = menus.find_menu(
-        "LevelEditor.LevelEditorToolBar.PlayToolBar"
-    )
-    level_menu_bar.add_section(section_name=section_name, label=section_name)
-
-    entry = unreal.ToolMenuEntry(type=unreal.MultiBlockType.TOOL_BAR_BUTTON)
-    entry.set_label("Ayon Toolbox")
-    entry.set_tool_tip("Ayon Toolbox")
-    entry.set_icon("EditorStyle", "Symbols.SearchGlass")
-    entry.set_string_command(
-        type=unreal.ToolMenuStringCommandType.PYTHON,
-        custom_type=unreal.Name(""),
-        string=command,
-    )
-    level_menu_bar.add_menu_entry(section_name, entry)
-    menus.refresh_all_widgets()
+    AyonToolsMenuTool(ayon_menu)
